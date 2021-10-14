6e6aba23d00f68eb5b44a7574f329fd8af222fb3
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-10-14 09:54:07 +0530

    add project

---
+++ lib/player/music_player.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MusicPlayer extends StatefulWidget {
+  const MusicPlayer({Key? key}) : super(key: key);
+
+  @override
+  _MusicPlayerState createState() => _MusicPlayerState();
+}
+
+class _MusicPlayerState extends State<MusicPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
