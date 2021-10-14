8113d5d892606b04647b8bfdca08cc4fbba9b069
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-10-14 10:02:54 +0530

    add utils"

--- lib/player/music_player.dart
+++ lib/player/music_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/utils/app_colors.dart';
 
 class MusicPlayer extends StatefulWidget {
   const MusicPlayer({Key? key}) : super(key: key);
@@ -11,6 +12,9 @@ class _MusicPlayerState extends State<MusicPlayer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kwhite,
+      ),
       body: Container(),
     );
   }
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff4d7aff);
+const darkPrimaryColor = Color(0xff3655b3);
+const lightPrimaryColor = Color(0xff4dffff);
+const greyColor = Color(0xffa488b3);
+const darkGreyColor = Color(0xff838383);
+const kwhite = Colors.white;
+const fontColor = Colors.black;
+const kblue = Colors.blue;
+const kBlack = Colors.black;
+const lightGreen = Color(0xff36b3b3);
